bd5c3187a3ad299a0fdc511ea754705025cda289
mathew461 <mathew461@gmail.com>
2021-03-06 14:06:06 +0000

    [Daily Commit 04/03/2021 - Return to this if next tests break code]

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'gatsby';
 import Logo from '../links-and-images/logo.png';
 
 
@@ -10,13 +11,13 @@ const Header = () => {
             <nav>
                 <ul className = "right header_nav">
                     <li className = "header_nav__list">
-                        <a className = "header_nav__button" href="/index.js">Gallery</a>
+                        <Link className = "header_nav__button" to="/">Gallery</Link>
                     </li>
                     <li className = "header_nav__list">
-                        <a className = "header_nav__button" href="/about.js">About Me</a>
+                        <Link className = "header_nav__button" to="/about">About Me</Link>
                     </li>
                     <li className="header_nav__list mobile-nav__item--cta">
-                        <a className = "header_nav__button" href="/contact.html">Contact Us</a>
+                        <Link className = "header_nav__button" to="/contact">Contact Us</Link>
                     </li>
                 </ul>
             </nav>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Header from '../components/Header'
+import Footer from '../components/Footer';
+
+
 
 const Contact = () => {
     return (
-        <h1>
-            hihi
-        </h1>
+        <div>
+            <Header />
+    
+            <Footer />
+        </div>
+        
     )
 }
 
