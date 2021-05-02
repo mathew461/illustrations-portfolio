d081b5a04ebc1ab687b890d3133be8e32dc433a7
mathew461 <mathew461@gmail.com>
2021-05-02 21:50:31 +0100

    [Fixed Lightbox issue! It is now working perfectly, had to change a function to a class to utilise componentdidmount]

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -16,32 +16,55 @@ import MonkeyAstronaut from '../links-and-images/Monkey-Astronaut.png'
 import RebelliousGnome from '../links-and-images/Rebellious-Gnome.png'
 import PokemonConcept from '../links-and-images/Pokemon-Concept.png'
 
-const galleryImages = [
-    AnimalKingdom1, AnimalKingdom2, AnimalKingdom3, AnimalKingdom4, 
-    AnimalKingdom5, AnimalKingdom6, ChocoboPrototype, KillTheBear,
-    GasmaskCat, MonkeyAstronaut, RebelliousGnome, PokemonConcept 
-]
 
 
-const Gallery = (props) => {
-    return (
-        <div>
-            <div className = "grid">
-                <ImageCard src = {ChocoboPrototype}/>
-                <ImageCard src = {AnimalKingdom1}/> 
-                <ImageCard src = {MonkeyAstronaut}/>
-                <ImageCard src = {AnimalKingdom2}/>  
-                <ImageCard src = {AnimalKingdom3}/> 
-                <ImageCard src = {RebelliousGnome}/> 
-                <ImageCard src = {AnimalKingdom4}/> 
-                <ImageCard src = {AnimalKingdom5}/> 
-                <ImageCard src = {PokemonConcept}/> 
-                <ImageCard src = {AnimalKingdom6}/>
-                <ImageCard src = {KillTheBear}/> 
-                <ImageCard src = {GasmaskCat}/>  
+class Gallery extends React.Component {
+    componentDidMount() {
+                // This is the Lightbox code from the HTML version of this site. It works perfect there, but here seems to be causing problems.                              
+                const lightbox = document.createElement('div')
+                lightbox.id = 'lightbox'
+                document.body.appendChild(lightbox)
+        
+                const images = document.querySelectorAll('img')
+                images.forEach(image => {
+                    image.addEventListener('click', e => {
+                        lightbox.classList.add('active')
+                        const img = document.createElement('img')
+                        img.src = image.src
+                        while (lightbox.firstChild) {
+                            lightbox.removeChild(lightbox.firstChild)
+                        }
+                        lightbox.appendChild(img)
+                    })
+                });
+        
+                lightbox.addEventListener('click', e => {
+                    if (e.target !== e.currentTarget) return
+                    lightbox.classList.remove('active')
+                })
+                //End of Lightbox Code
+    }
+
+    render() {
+        return (
+            <div>
+                <div className = "grid">
+                    <ImageCard src = {ChocoboPrototype}/>
+                    <ImageCard src = {AnimalKingdom1}/> 
+                    <ImageCard src = {MonkeyAstronaut}/>
+                    <ImageCard src = {AnimalKingdom2}/>  
+                    <ImageCard src = {AnimalKingdom3}/> 
+                    <ImageCard src = {RebelliousGnome}/> 
+                    <ImageCard src = {AnimalKingdom4}/> 
+                    <ImageCard src = {AnimalKingdom5}/> 
+                    <ImageCard src = {PokemonConcept}/> 
+                    <ImageCard src = {AnimalKingdom6}/>
+                    <ImageCard src = {KillTheBear}/> 
+                    <ImageCard src = {GasmaskCat}/>  
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Gallery;
