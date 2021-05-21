0ac0cd9441d9b8c8ddeccb942530ec522a9b6714
mathew461 <mathew461@gmail.com>
2021-05-21 17:01:02 +0100

    [updated the about page with a second draft! reads a lot better and much less robotic! Also fixed the display picture not being properly centered.]

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,23 +8,25 @@ const About = () => {
     return (
         <div>
             <Header />
+           <br/>
             <img 
-                src={DisplayPic}  
-                alt="This is me" 
-                class="display_pic"
+                src = {DisplayPic}  
+                alt = "This is me" 
+                className = "display_pic"
             />
             
             <div class="centre">
-                <h2 class="lower_h2">Bio</h2>
-                <p class = "lower_p">Mathew Wrigley is a freelance illustrator specializing in character design, games design [think of more to specialize in] At an early age he was drawing and creating, prompting an interest in the fine arts in high school and college and went on to study Computer Games Design at University.</p>
-                <p class = "lower_p">Most of Mathew's work begins by sketching ideas in an art book, and then they are enhanced into illustrations via using Adobe software such as Illustrator and Photoshop.</p>
-                <p class = "lower_p">-Please feel free to contact Mathew for commissions and inquiries-</p>
-                <h2 class="lower_h2">Awards</h2>
-                <p class = "lower_p">2013 – Games Republic Contest, for University Team Project. –Judges included popular games companies such as: The Four Door Lemon, Just Add Water and The Blast Furnace.</p>
-                <p class = "lower_p">Mathew's Team won three awards, such as </p>
-                <li class = "lower_p">First place for Game Art</li>
-                <li class = "lower_p">Third Place for Game Technology</li>
-                <li class = "lower_p">Third Place for Teamwork</li>
+                <h2 className = "lower_h2">Bio</h2>
+                <p className = "lower_p"><b>Hi There!</b> my name is Mathew Wrigley, I am a freelance illustrator based in the UK, with some knowledge of web development, 3d modelling for Games, concept design, and many other odd and wonderful skills one makes during life in the 21st century.</p>
+                <p className = "center_text">◆    ◆    ◆</p>
+                <p className = "lower_p"> I studied Computer Games Design at Huddersfield University between 2009-2013, where my colourful work was recognised by prestigious games companies such as The Four Door Lemon, Just Add Water and The Blast Furnace. At the Games Republic Contest in 2013. Myself and my team all won awards for Game Art, Game Technology, and Best Team. </p>
+                <p className = "center_text">◆    ◆    ◆</p>        
+                <p className = "lower_p">When I’m not spending my time drawing pretty pictures, I am usually off procrastinating, playing video games or pretending I’m having a conversation with my miniature Jack Russell Alfie.</p>
+                <p className = "center_text">◆    ◆    ◆</p>
+                <p className = "lower_p">Since my time at university I travelled a bit, lived in Toronto Canada, where I spent most of my time learning the complexities of Coffee. After that I travelled Australia, met a kangaroo but now I have returned to my homeland, England and I very much plan to plant my butt firmly down and concentrate on my career.</p>
+                <p className = "center_text">◆    ◆    ◆</p>
+                <p className = "lower_p">This space is a place for me to showcase my designs, everything you see here I created, even the site itself, I can code too. </p>
+                
             </div>
             
             <div class="bottom"></div>
