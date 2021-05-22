ee08f3f205b8309f11646e86a39d9a4ff0de23d1
mathew461 <mathew461@gmail.com>
2021-05-22 12:39:38 +0100

    [Rebuilding the Contact Form to make it functional with Netlify]

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer';
 
-
-
 const Contact = () => {
     return (
         <div>
             <Header />
-    
+            <form 
+                name="contact" 
+                method="post"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+            >
+                <input name="name" placeholder="Your Name" type="text"/>
+                <button>Send</button>
+            </form>
             <Footer />
         </div>
         
