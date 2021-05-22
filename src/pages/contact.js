0b89a07224f7bf58f92c7d752ee528c6c06ed368
mathew461 <mathew461@gmail.com>
2021-05-22 13:19:12 +0100

    [Contact Form second Draft]

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,19 +6,31 @@ const Contact = () => {
     return (
         <div>
             <Header />
-            <form 
-                name="contact" 
-                method="post"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-            >
-                <input name="name" placeholder="Your Name" type="text"/>
-                <button>Send</button>
+            <form name="contact" method="POST" data-netlify="true">
+                <p>
+                    <label>Your Name: <input type="text" name="name" /></label>   
+                </p>
+                <p>
+                    <label>Your Email: <input type="email" name="email" /></label>
+                </p>
+                <p>
+                    <label>Your Role: <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                    </select></label>
+                </p>
+                <p>
+                    <label>Message: <textarea name="message"></textarea></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
             </form>
             <Footer />
         </div>
-        
     )
 }
 
+
+            
 export default Contact;
