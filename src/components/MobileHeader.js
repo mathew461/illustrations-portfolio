3a2daa63f11ccb8823566cf6b3538dabff74774e
mathew461 <mathew461@gmail.com>
2021-08-04 18:37:43 +0100

    [Made Header responsive, still needs to be positioned, but Hamburger and Desktop Nav work together]

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -10,74 +10,82 @@ const Global = createGlobalStyle`
   }
 `
 const MenuIcon = styled.button`
-  position: fixed;
-  top: 2rem;
-  right: 2rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  height: 1.5rem;
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  z-index: 5;
+  display: none;  
 
-  div {
-    width: 1.5rem;
-    height: 0.2rem;
-    background: black;
-    border-radius: 5px;
-    transform-origin: 1px;
-    position: relative;
-    transition: opacity 300ms, transform 300ms;
+  @media (max-width: 40rem) {
+    position: fixed;
+    top: 2rem;
+    right: 2rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 1.5rem;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    z-index: 5;
 
-    :first-child {
-      transform: ${({nav}) => nav ? 'rotate(45deg)' : 'rotate(0)' } 
-    }
+    div {
+      width: 1.5rem;
+      height: 0.2rem;
+      background: black;
+      border-radius: 5px;
+      transform-origin: 1px;
+      position: relative;
+      transition: opacity 300ms, transform 300ms;
 
-    :nth-child(2) {
-      opacity: ${({nav}) => nav ? "0" : "1"}
-    }
-    :nth-child(3) {
-      transform: ${({nav}) => nav ? 'rotate(-45deg)' : 'rotate(0)'}
+      :first-child {
+        transform: ${({nav}) => nav ? 'rotate(45deg)' : 'rotate(0)' } 
+      }
+
+      :nth-child(2) {
+        opacity: ${({nav}) => nav ? "0" : "1"}
+      }
+      :nth-child(3) {
+        transform: ${({nav}) => nav ? 'rotate(-45deg)' : 'rotate(0)'}
+      }
     }
   }
 `
 const MenuLinks = styled.nav`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  height: 100vh;
-  width: 25%;
-  transition: transform 300ms; 
-  background: #d7d7d7;
-  position: absolute;
-  top: 0;
-  right: 0;
-  transform: ${({ nav }) => (nav ? "translateX(0)" : "translateX(100%)")};
+  display: none;
 
-  ul {
-    list-style-type: none;
-  }
+  @media (max-width: 40rem) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100vh;
+    width: 25%;
+    transition: transform 300ms; 
+    background: #d7d7d7;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: ${({ nav }) => (nav ? "translateX(0)" : "translateX(100%)")};
 
-  li {
-    margin-top: 1rem;
-  }
+    ul {
+      list-style-type: none;
+    }
 
-  .linkStyle {
-    text-decoration: none;
-    color: red;
-    font-size: 1.5rem;
+    li {
+      margin-top: 1rem;
+    }
 
-    :hover {
-      color: blue;
+    .linkStyle {
+      text-decoration: none;
+      color: #990f3d;
+      font-size: 1.5rem;
+
+      :hover {
+        color: black;
+      }
     }
   }
 `
 
-const Hamburger = () => {
+const MobileHeader = () => {
   const [nav, showNav] = useState(false)
 
   return (
@@ -91,22 +99,22 @@ const Hamburger = () => {
       <MenuLinks nav={nav}>
         <ul>
           <li>
-            <Link className="linkStyle" href="#">Home</Link>
+            <Link className="linkStyle" to="/">Home</Link>
           </li>
         </ul>
         <ul>
           <li>
-            <Link className="linkStyle" href="#">Gallery</Link>
+            <Link className="linkStyle" to="../theGallery">Gallery</Link>
           </li>
         </ul>
         <ul>
           <li>
-            <Link className="linkStyle" href="#">About Me</Link>
+            <Link className="linkStyle" to="../about">About Me</Link>
           </li>
         </ul>
         <ul>
           <li>
-            <Link className="linkStyle" href="#">Contact Us</Link>
+            <Link className="linkStyle" to="../contact">Contact Us</Link>
           </li>
         </ul>
       </MenuLinks>
@@ -114,4 +122,4 @@ const Hamburger = () => {
   )
 }
 
-export default Hamburger;
+export default MobileHeader;
